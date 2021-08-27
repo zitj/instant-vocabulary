c4f54e6a16d78ef0c9713d2d15b06b7882c4002c
unknown <ognjenveljkovic3@gmail.com>
2021-08-27 11:08:49 +0200

    27/08/2021 (12)

--- src/data/vocabulary.js
+++ src/data/vocabulary.js
@@ -14,7 +14,7 @@ export const vocabulary = [
 	{
 		id: 3,
 		word: 'abakus',
-		meaning: `l. svečani stoza skupoceno posuđe kod starih Rimljana; 2.daska za igru; 3. sto posut peskom zamatematičke crteže i radove, daska na kojojse računa, računaljke; 4. u kat. crkvi: stopored oltara; 5.arh.četvrtasta ploča kojapokriva glavicu(kapitl)stuba.`,
+		meaning: `l. svečani sto za skupoceno posuđe kod starih Rimljana; 2.daska za igru; 3. sto posut peskom za matematičke crteže i radove, daska na kojoj se računa, računaljke; 4. u kat. crkvi: stopored oltara; 5.arh.četvrtasta ploča kojapokriva glavicu(kapitl)stuba.`,
 		origin: 'lat. abacus, grč. abaks',
 	},
 	{
@@ -25,9 +25,75 @@ export const vocabulary = [
 	},
 	{
 		id: 5,
-		word: '',
-		meaning: '',
-		origin: '',
+		word: 'abalijenacija',
+		meaning: 'prav.otuđivanje,otuđenje, odstranjivanje, odstra-njenje.',
+		origin: 'lat. abalienatio',
+	},
+	{
+		id: 6,
+		word: 'abalijenirati',
+		meaning: 'prav.lišiti, otuđiti,otuđivati; fig.odvratiti koga od nekoga,otuđiti.',
+		origin: 'lat. abalienare',
+	},
+	{
+		id: 7,
+		word: 'abanacija',
+		meaning: 'prav.progonstvo iz zemlje na jednu godinu.',
+		origin: 'lat. ab od, annus godina',
+	},
+	{
+		id: 8,
+		word: 'abandon',
+		meaning: 'odstupanje, odricanje odsvog prava; pravo osiguranika da osigu-račuustupi robu ili brod i sve na njemu uz isplatu iznosa na koji su roba ili brod bili osigurani. abas (pere.) jedinica za merenje bisera u Persiji,nešto manja od harata=OD458 g.',
+		origin: 'fr. abandon',
+	},
+	{
+		id: 9,
+		word: 'abasimento',
+		meaning: 'snižavanje,smanjivanje, obaranje, spuštanje.',
+		origin: 'ital. abassimento',
+	},
+	{
+		id: 10,
+		word: 'abasimento di voče',
+		meaning: 'muz.spuštanje glasa.',
+		origin: 'ital. abassimento divoće',
+	},
+	{
+		id: 11,
+		word: 'abasimento di mano',
+		meaning: 'muz.spuštanje ruke pri udaranju takta; metanje jedne ruke pod drugu (prisviranju na klaviru).',
+		origin: 'ital. abassimento dimano',
+	},
+	{
+		id: 12,
+		word: 'abasimento di preco',
+		meaning: 'trg. spuštanje cene, obaranje cene.',
+		origin: 'ital. abassimento diprezzo',
+	},
+	{
+		id: 13,
+		word: 'abat',
+		meaning: 'opat, iguman, starešina manastira; up. abe.',
+		origin: 'ital. abate',
+	},
+	{
+		id: 14,
+		word: 'abataža',
+		meaning: 'seča drva; klanje ili ubijanje stoke;fig.oštar ukor',
+		origin: 'fr. abbattage',
+	},
+	{
+		id: 15,
+		word: 'abaton',
+		meaning: 'deo hrama,svetilišta ili crkve u koji ne smeju ući obični vernici, inoverci i svetovnja-ci; up. aditon.',
+		origin: 'grč. abatos nepristupačan',
+	},
+	{
+		id: 16,
+		word: 'a batuta',
+		meaning: 'muz.prema udarutakta, po tempu.',
+		origin: 'ital. a battuta',
 	},
 	{
 		id: ,
@@ -39,6 +105,11 @@ export const vocabulary = [
 /*
 
  
+a batuta
+(ital. a battuta)
+muz.prema udarutakta, po tempu.
+
+
 
 
 
