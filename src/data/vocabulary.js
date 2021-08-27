6af9380879cebc0854a49d01fbade27f8f9f36f5
unknown <ognjenveljkovic3@gmail.com>
2021-08-27 10:38:52 +0200

    27.08.2021 (3)

--- src/data/vocabulary.js
+++ src/data/vocabulary.js
@@ -5,8 +5,41 @@ export const vocabulary = [
 		meaning: 'štit na lampi od hartije, tkanine i dr. za zaklanjanje očiju od svetlosti.',
 		origin: 'fr. abat-jour',
 	},
+	{
+		id: 2,
+		word: 'abazija',
+		meaning: 'nesposobnost hodanja.',
+		origin: 'grč. a- ne, basis korak',
+	},
+	{
+		id: 3,
+		word: 'abakus',
+		meaning: `l. svečani stoza skupoceno posuđe kod starih Rimljana; 2.daska za igru; 3. sto posut peskom zamatematičke crteže i radove, daska na kojojse računa, računaljke; 4. u kat. crkvi: stopored oltara; 5.arh.četvrtasta ploča kojapokriva glavicu(kapitl)stuba.`,
+		origin: 'lat. abacus, grč. abaks',
+	},
+	{
+		id: 4,
+		word: 'abakcija',
+		meaning: 'med.pobačaj,pobacivanje, pomet; up. abortus.',
+		origin: 'lat. abactio partus',
+	},
+	{
+		id: 5,
+		word: '',
+		meaning: '',
+		origin: '',
+	},
+	{
+		id: ,
+		word: '',
+		meaning: '',
+		origin: '',
+	},
 ];
 /*
+
+ 
+
 
 
     */
